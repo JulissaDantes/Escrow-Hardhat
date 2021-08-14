5ca628a449e3b9f778b6e514ce0504e344e98ff6
JulissaDantes <julissa@singularsound.com>
2021-08-14 18:17:56 -0400

    Fixes #4

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,15 @@ module.exports = {
     },
     ganache: {
       url: "HTTP://127.0.0.1:7545"
+    },
+    rinkeby: {
+      url:"https://rinkeby.infura.io/v3/433a74c66045425ba8fdf7f1cb23ffdb"
+    },
+    kovan: {
+      url:"https://kovan.infura.io/v3/433a74c66045425ba8fdf7f1cb23ffdb"
+    },
+    ropsten:{
+      url:"https://ropsten.infura.io/v3/433a74c66045425ba8fdf7f1cb23ffdb"
     }
   },
   paths: {

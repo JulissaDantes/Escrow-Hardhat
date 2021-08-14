ec023b48ac3dfb2f54f3076a9c5862bedc99a7ec
JulissaDantes <julissa@singularsound.com>
2021-08-14 18:10:35 -0400

    Fixes #9

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,15 @@ require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.7.5",
+  networks: {
+    hardhat: {
+    },
+    ganache: {
+      url: "HTTP://127.0.0.1:7545"
+    }
+  },
   paths: {
     artifacts: "./app/artifacts",
   }
+
 };
